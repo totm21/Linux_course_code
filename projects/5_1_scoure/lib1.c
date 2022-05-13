d3112088adf02fc70ac2d9f28097b79ae1e61016
Seemingly <2781343172@qq.com>
2022-05-13 17:01:34 +0800

    Update lib1.c

--- projects/5_1_scoure/lib1.c
+++ projects/5_1_scoure/lib1.c
@@ -8,12 +8,13 @@ void input(Student **students,int n)
     {
         printf("please input %d student-name:",i+1);
         scanf("%s",students[i]->name);
+        //scanf("%s",students[i]->name);
         printf("please input %d student-age:",i+1);
-        scanf("%d",&students[i]->age);
+        scanf("%d",&(*students)[i].age);
         printf("please input %d student-ch:",i+1);
-        scanf("%f",&students[i]->ch);
+        scanf("%f",&(*students)[i].ch);
         printf("please input %d student-math:",i+1);
-        scanf("%f",&students[i]->math);
+        scanf("%f",&(*students)[i].math);
     }
     return ;
 }
